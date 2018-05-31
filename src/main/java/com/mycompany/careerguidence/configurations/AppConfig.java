8e88ed0d04290602ecf252ae3e3a937e3f9e757c
Ilya Robbot <rickmortyand4@gmail.com>
2018-05-31 14:21:58 +0300

    Add Config

---
+++ src/main/java/com/mycompany/careerguidence/configurations/AppConfig.java
@@ -0,0 +1,13 @@
+package com.mycompany.careerguidence.configurations;
+
+import org.springframework.context.annotation.ComponentScan; 
+import org.springframework.context.annotation.Configuration; 
+import org.springframework.web.servlet.config.annotation.EnableWebMvc; 
+
+@Configuration 
+@ComponentScan(basePackages = "com.mycompany.careerguidence") 
+@EnableWebMvc 
+
+public class AppConfig {
+ 
+}
